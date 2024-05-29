28ad7f933f655ea403130d2da05cb4fd811edc50
ThakurOmSingh <105989319+ThakurOmSingh@users.noreply.github.com>
2024-05-29 21:55:53 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ import { Router } from "express"
 import { register, login, getUser, forgetPassword } from '../controller/userController.js'
 import apiLimiter from "../middleware/apiLimiter.js"
 const router = Router()
-apiLimiter
+
 router.post('/register', apiLimiter, register)
 router.post('/login',apiLimiter, login)
 router.get('/profile',apiLimiter, getUser)
